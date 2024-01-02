65c3b6e116cdf2df10cfa3fff4e46db9797a508b
alfimiftanur <alfinurhakim1234@gmail.com>
2024-01-03 00:38:01 +0700

    memperbaiki enable dan disable scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 // DISABLE SCROLL HERO/HOME
 const root_element = document.querySelector(":root");
-function disbale_scroll(){
-    scroll_top = (window.pageYOffset) || document.documentElement.scroll_top;
-    scroll_left = (window.pageXOffset) || document.documentElement.scroll_left;
+function disable_scroll(){
+  scroll_top = (window.pageYOffset) || document.documentElement.scroll_top;
+  scroll_left = (window.pageXOffset) || document.documentElement.scroll_left;
 
     window.onscroll = function(){
         window.scrollTo(scroll_top, scroll_left);
     }
 
     root_element.style.scrollBehavior ="auto";
+}
+
+function enable_scroll(){
+  window.onscroll = function(){ }
+  root_element.style.scrollBehavior ="smooth";
+  localStorage.setItem('opened', 'true');
+}
+
+if(!localStorage.getItem('opened')){
+  disable_scroll()
 }
 
 // LOADING AFTER SEND 
