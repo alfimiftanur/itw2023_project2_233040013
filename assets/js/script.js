953b0adf75c69702f73c460b980eff712b3edf6a
alfimiftanur <alfinurhakim1234@gmail.com>
2024-01-03 00:23:11 +0700

    mengaktifkan contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,38 @@ function disbale_scroll(){
     root_element.style.scrollBehavior ="auto";
 }
 
-function enable_scroll(){
-    window.onscroll = function(){ }
-    root_element.style.scrollBehavior ="smooth";
-    localStorage.setItem('opened', 'true');
-}
+// LOADING AFTER SEND 
+function submitForm(event) {
+    event.preventDefault();
+  
+    // Disable true mencegah untuk mengirim berkali-kali
+    document.getElementById('submit-button').disabled = true;
+  
+    // Change the content of the submit button to include the loading animation
+    document.getElementById('submit-button').innerHTML = '<div id="loading-spinner"></div>';
+  
+    setTimeout(function() {
+      // Re-enable the submit button
+      document.getElementById('submit-button').disabled = false;
+      document.getElementById('submit-button').innerHTML = 'Send';
+      document.getElementById('success-popup').style.display = 'flex';
+    }, 2899);
+  }
+  
+  function closePopup() {
+    document.getElementById('success-popup').style.display = 'none';
+  }
+  
+// INPUT TO SPREADSHEET
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz_hZLFhEFZ4Clna_YVhXrs2rY9EmLYZlK9JVJEoR5L24PuQSYTmpQUKjDg-hVP925M/exec'
+const form = document.forms['contact-form']
 
-if(!localStorage.getItem('opened')){
-    disbale_scroll();
-}
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        form.reset()
+        console.log('Success!', response)
+    })
+    .catch(error => console.error('Error!', error.message))
+})
